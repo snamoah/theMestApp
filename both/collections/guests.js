38342ebf3ccfd64407ca53fd1c2cb4dcc83f750a
Samuel Amoah <snamoah@gmail.com>
2015-03-03 19:26:31 +0000

    Guests collection added

---
+++ both/collections/guests.js
@@ -0,0 +1,22 @@
+Guests = new Mongo.Collection('guests');
+
+Guests.attachSchema( new SimpleSchema({
+  name: {
+    type: String
+  },
+
+  bio: {
+    type: String,
+    autoform: {
+      rows: 5
+    }     
+  },
+
+  date: {
+    type: String
+  },
+
+  type: {
+    type: String
+  }
+}));
